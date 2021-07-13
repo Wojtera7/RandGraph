b8009e06d513303eb0b3e1be4defc0afb365df66
AsRock <AsRock@MICHALROCK>
2021-07-13 13:05:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RandGraph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
